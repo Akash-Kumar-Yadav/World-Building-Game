ad40dbc84283cd09efe5997a8490afac45741f34
Akash-Kumar-Yadav <akashkumaryadav7411@gmail.com>
2021-07-06 23:36:49 +0530

    Character controller Done..
    Added third person character controller for PC using Cinemachine..

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] CharacterController characterController;
+    [SerializeField] Animator animator;
+    [SerializeField] float speed = 10f;
+    [SerializeField] Transform cam;
+    [SerializeField] float turnSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        var right = cam.right;
+        var forward = cam.forward;
+
+        right.y = 0;
+        forward.y = 0;
+
+        right.Normalize();
+        forward.Normalize();
+
+        Vector3 move = forward * v + right * h;
+        Quaternion rot = Quaternion.LookRotation(move);
         
+        characterController.Move(move * speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, turnSpeed * Time.deltaTime);
+       
+        animator.SetFloat("speed", move.magnitude);
     }
 }
